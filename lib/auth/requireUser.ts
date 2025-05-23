136604aafc44678973e4dd20a13ab3940e76e06c
Aleksandar <aleksandar@e-vlasotince.info>
2025-05-23 12:33:33 +0200

    requireUser function change

--- lib/auth/requireUser.ts
+++ lib/auth/requireUser.ts
@@ -1,32 +1,18 @@
-import { verifyJWT } from "@/lib/auth/jwt";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { verifyJWT } from "./jwt";
 
 export async function requireUser(
   request: NextRequest
-): Promise<string | NextResponse> {
+): Promise<string | null> {
   const authHeader = request.headers.get("authorization");
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return NextResponse.json(
-      { error: "Nemate prava pristupa profilu" },
-      { status: 401 }
-    );
-  }
+  if (!authHeader?.startsWith("Bearer ")) return null;
 
   try {
     const token = authHeader.split(" ")[1];
-    const payload = await verifyJWT(token);
-
-    if (!payload?.userId) {
-      throw new Error("Nedostaje korisnički ID u tokenu");
-    }
-
-    return payload.userId as string;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (err) {
-    return NextResponse.json(
-      { error: "Nevažeći ili istekao token.Molimo prijavite se ponovo" },
-      { status: 401 }
-    );
+    const payload = (await verifyJWT(token)) as { userId: string } | null;
+    return payload?.userId ?? null;
+  } catch {
+    return null;
   }
 }
